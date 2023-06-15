c69c1d9467c5ee3031f233c50899f7d53750cff8
Hemil Bhavsar <hemilbhavsar@gmail.com>
2023-06-15 10:26:02 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression=require('compression');
-
+const cors=require('cors');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -24,10 +24,16 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.set('Content-Type', 'application/javascript');
 // 1) GLOBAL MIDDLEWARES
+
+//Implement cors
+app.use(cors());
+
+app.options('/api/v1/tours/:id',cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
+
 //app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
